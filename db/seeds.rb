aecf40880fcd44aaece7b0e63fa9f426fc51c9f6
maya garcia <maya.garciabo@gmail.com>
2024-06-05 21:33:51 -0600

    Add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Movie.destroy_all
+List.destroy_all
+Bookmark.destroy_all
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies = JSON.parse(URI.open(url).read)
+
+movies["results"].each do |movie|
+  data = {
+    title: movie["title"],
+    overview: movie["overview"],
+    rating: movie["vote_average"],
+    poster_url: "https://image.tmdb.org/t/p/w200/#{movie["poster_path"]}"
+  }
+  Movie.create!(data)
+end
